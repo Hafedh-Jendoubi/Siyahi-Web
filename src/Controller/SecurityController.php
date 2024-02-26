c20ad1e1b104ae067b7e69ba7c66ea269908a288
Hafedh-Jendoubi <hafedh.jendoubi@esprit.tn>
2024-02-26 23:14:21 +0100

    Reset Password added && Blocked users have no access anymore

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
