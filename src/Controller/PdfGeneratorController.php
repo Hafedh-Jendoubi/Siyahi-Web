5a01c13cdcd3bb86d347c977a35f3e645b2ef0ef
Hafedh-Jendoubi <hafedh.jendoubi@esprit.tn>
2024-02-28 16:12:52 +0100

    QR Code added to the PDF File

--- src/Controller/PdfGeneratorController.php
+++ src/Controller/PdfGeneratorController.php
@@ -3,6 +3,12 @@
 namespace App\Controller;
 
 use App\Repository\UserRepository;
+use Endroid\QrCode\Color\Color;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\Label\Font\NotoSans;
+use Endroid\QrCode\Label\Label;
+use Endroid\QrCode\QrCode;
+use Endroid\QrCode\Writer\PngWriter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,6 +20,15 @@ class PdfGeneratorController extends AbstractController
     public function index($id, UserRepository $repository): Response
     {
         $user = $repository->find($id);
+        $writer = new PngWriter();
+        $qrCode = QrCode::create('http://127.0.0.1:8000/account/' . $id)
+            ->setEncoding(new Encoding('UTF-8'))
+            ->setSize(120)
+            ->setMargin(0)
+            ->setForegroundColor(new Color(0, 0, 0))
+            ->setBackgroundColor(new Color(255, 255, 255));
+        $label = Label::create('')->setFont(new NotoSans(8));
+        $qrCode = $writer->write($qrCode, null)->getDataUri();
         $data = [
             'LogoSrc'=> $this->imageToBase64($this->getParameter('kernel.project_dir') . '/public/front/assets/img/s-logo.png'),
             'ImageSrc'  => $this->imageToBase64($user->getImage()),
@@ -21,18 +36,16 @@ class PdfGeneratorController extends AbstractController
             'LastName'         => $user->getLastName(),
             'Address'      => $user->getAddress(),
             'PhoneNumber' => "(+216) " . $user->getPhoneNumber(),
-            'CIN' => "(+216) " . $user->getCIN(),
-            'Email'        => $user->getEmail()
+            'CIN' => $user->getCIN(),
+            'Email'        => $user->getEmail(),
+            'QR' => $qrCode
         ];
         $html =  $this->renderView('pdf_generator/index.html.twig', $data);
         $dompdf = new Dompdf();
         $dompdf->loadHtml($html);
         $dompdf->render();
-        return new Response (
-            $dompdf->stream('resume', ["Attachment" => false]),
-            Response::HTTP_OK,
-            ['Content-Type' => 'application/pdf']
-        );
+
+        return new Response($dompdf->stream('resume', ["Attachment" => false]), Response::HTTP_OK, ['Content-Type' => 'application/pdf']);
     }
 
     private function imageToBase64($path): string
