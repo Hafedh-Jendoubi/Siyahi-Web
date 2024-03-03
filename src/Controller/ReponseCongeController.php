c23b82767cac72ce5ca7c596cd1ebcef444d147b
ahmedbettaieb20 <bettaieb.ahmed1@esprit.tn>
2024-03-03 02:01:37 +0100

    3eme metier

--- src/Controller/ReponseCongeController.php
+++ src/Controller/ReponseCongeController.php
@@ -12,6 +12,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Security;
+use Dompdf\Dompdf;
+use Dompdf\Options;
 
 
 
@@ -102,4 +104,54 @@ public function delete(ManagerRegistry $managerRegistry, $id, ReponseCongeReposi
 
     return $this->redirectToRoute('app_reponse_conge_index');
 }
+
+    #[Route('/pdf/{id}', name: 'app_reponse_conge_pdf')]
+    public function generatePdf( int $id,ReponseCongeRepository $reponseCongeRepository): Response
+    {
+        $reponseConges = $reponseCongeRepository->find($id);
+        $data = [
+            
+            
+            'description'         => $reponseConges->getDescription(),
+            'DateDebut'         => $reponseConges->getDateDebut(),
+            'DateFin' => $reponseConges->getDateFin(),
+            'Conge'      => $reponseConges->getConge(),
+            
+        ];
+
+        // Création d'une instance Dompdf
+        $options = new Options();
+        $options->set('isHtml5ParserEnabled', true);
+        $options->set('isRemoteEnabled', true);
+        $dompdf = new Dompdf($options);
+
+        // Construction du contenu HTML du PDF
+        $html =  $this->renderView('reponse_conge/pdf.html.twig', $data);
+
+        // Chargement du contenu HTML dans Dompdf
+        $dompdf->loadHtml($html);
+
+        // Réglage du format du papier et du style (optionnel)
+        $dompdf->setPaper('A4', 'portrait');
+
+        // Rendu du PDF
+        $dompdf->render();
+
+        // Envoi du PDF en réponse
+        return new Response(
+            $dompdf->output(),
+            Response::HTTP_OK,
+            [
+                'Content-Type' => 'application/pdf',
+            ]
+        );
+    }
+    private function imageToBase64($path): string
+    {
+        $type = pathinfo($path, PATHINFO_EXTENSION);
+        $data = file_get_contents($path);
+        $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
+        return $base64;
+    }
+
 }
