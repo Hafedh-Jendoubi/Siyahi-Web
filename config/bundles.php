dd7eeb9788af93f48e328cb592e66c9ca9ff5746
Imedimed11 <khlifi.imededdine@esprit.tn>
2024-03-06 12:56:10 +0100

    Pushing latest modifications

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,6 @@ return [
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Endroid\QrCodeBundle\EndroidQrCodeBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
 ];
--- src/Controller/AchatController.php
+++ src/Controller/AchatController.php
@@ -10,15 +10,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Knp\Component\Pager\PaginatorInterface;
 #[Route('/achat')]
 class AchatController extends AbstractController
 {
     #[Route('/', name: 'app_achat_index', methods: ['GET'])]
-    public function index(AchatRepository $achatRepository): Response
+    public function index(AchatRepository $achatRepository ,PaginatorInterface $paginator,EntityManagerInterface $entityManager ,Request $request): Response
     {
+        $a= $entityManager->getRepository(Achat::class)->findAll();
+        $page = $paginator->paginate(
+            $a,
+            $request->query->getInt('page', 1),
+            3
+        );
         return $this->render('achat/index.html.twig', [
             'achats' => $achatRepository->findAll(),
+            'page' => $page
         ]);
     }
 
